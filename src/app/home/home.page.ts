e81c183b9bb4e42d04db7d144a22ed1c5a7b4c72
Mikhnova-Tatyana <tanjamikhnva@gmail.com>
2024-12-16 23:54:06 +0000

    updated content of Settings page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,16 +2,27 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonInput, IonButton } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { settingsOutline } from 'ionicons/icons';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonButton, IonInput, IonIcon, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonInput, IonIcon, IonHeader, IonToolbar, IonTitle, IonContent, FormsModule],
 })
 export class HomePage {
-  constructor() {
-    addIcons({ settingsOutline })
+  countryName: string = "";
+
+  constructor(private router: Router) {
+    addIcons({ settingsOutline });
+  }
+
+  searchCountry(){
+
+  }
+  openSettings(){
+    this.router.navigate(['/settings']);
   }
 }
